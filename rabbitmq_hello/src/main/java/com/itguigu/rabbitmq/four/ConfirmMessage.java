9a8dc9f846b3ba60a0fefdd7cdb47ee5041eccf1
luyuan <luyuan@egoonet.com>
2023-02-01 21:51:22 +0800

    处理异步消息确认方式未确认消息

--- rabbitmq_hello/src/main/java/com/itguigu/rabbitmq/four/ConfirmMessage.java
+++ rabbitmq_hello/src/main/java/com/itguigu/rabbitmq/four/ConfirmMessage.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConfirmCallback;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * @Author: IsaiahLu
@@ -84,31 +86,47 @@ public class ConfirmMessage {
         channel.queueDeclare(queueName, true, false, false, null);
         //开启发布确认
         channel.confirmSelect();
-        //开始时间：
-        long starTime = System.currentTimeMillis();
+        /**
+         * 准备线程安全有序的哈希表 适用于高并发场景
+         * 1序号与消息关联
+         * 2删除条目，只要给到序号
+         * 3支持高并发，（多线程）
+         */
+        ConcurrentSkipListMap<Long, String> outstandingConfirm = new ConcurrentSkipListMap<>();
 
         /**
          * 消息确认成功回调接口
          */
         ConfirmCallback ackCallback = (deliveryTag, multiple) -> {
-            System.out.println("确认的消息： " +deliveryTag);
+            System.out.println("确认的消息： " + deliveryTag);
+            //是否是批量
+            if (multiple) {
+                //删除确认的消息，剩余的就是未确认的消息
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirm.headMap(deliveryTag);
+                confirmed.clear();//批量清理
+            } else {//不是批量，单独删除
+                outstandingConfirm.remove(deliveryTag);
+            }
         };
         /**
          * 消息确认失败回调接口
          */
         ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
-            System.out.println("未确认的消息： " +deliveryTag);
+            //打印未确认的消息是那些
+            String message = outstandingConfirm.get(deliveryTag);
+            System.out.println("未确认的消息： " + message + ":::::未确认的消息tag： " + deliveryTag);
         };
         //监听器，监听那些消息成功了，那些消息失败了，
         channel.addConfirmListener(ackCallback, nackCallback);
 
+        //开始时间：
+        long starTime = System.currentTimeMillis();
         for (int i = 1; i <= 1000; i++) {
             String message = "消息 " + i;
             channel.basicPublish("", queueName, null, message.getBytes());
-
-
+            //记录所有要发送的消息，消息的总和
+            outstandingConfirm.put(channel.getNextPublishSeqNo(), message);
         }
-
         long endTime = System.currentTimeMillis();
         System.out.println("发布1000条消息，异步确认，" + "共耗时：" + (endTime - starTime) + "  毫秒");
 
